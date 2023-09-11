d728a4f2dab9252565ee16108ec42e2cadf8703d
adamokennie <adamokennie@gmail.com>
2023-09-11 03:00:33 +0100

    still on coding

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const dotenv=require("dotenv").config()
 const express=require("express")
 const mongoose=require("mongoose")
-const bodyparser=require("body-parser")
+const bodyParser=require("body-parser")
 const cors=require("cors")
 
 const app=express()
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json())
+//Routes
+app.get("/",(req,res)=>{
+    res.send("home page")
+})
+
+
 const PORT=process.env.PORT||5000
 mongoose.connect('mongodb+srv://kennie:869480Ak@cluster0.zkjbfkp.mongodb.net/pevents?retryWrites=true&w=majority'
 ,{
