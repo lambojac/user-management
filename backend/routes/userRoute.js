070493dee4c34176d736fdd190022bfa88461df0
adamokennie <adamokennie@gmail.com>
2023-09-23 23:57:42 +0100

    nn

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,6 +2,6 @@ const express=require('express')
 const { registerUser } = require('../controllers/userController')
 const router=express.Router()
 
-router.post("/register",registerUser)
+router.post("/registers",registerUser)
 
 module.exports=router
